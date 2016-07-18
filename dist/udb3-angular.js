79f508c9e9a2b9e719e4b99279e25cc6e4aa4384
Nick <nick@2dotstwice.be>
2016-07-18 09:22:15 +0200

    III-989: dist files

--- dist/udb3-angular.js
+++ dist/udb3-angular.js
@@ -2869,7 +2869,7 @@ function UdbApi(
       var permissions = offerCache.get(token);
       if (!permissions) {
         $http
-          .get(appConfig.baseUrl + 'user/permissions', defaultApiConfig)
+          .get(appConfig.baseUrl + 'user/permissions/', defaultApiConfig)
           .success(storeAndResolvePermissions)
           .error(deferredPermissions.reject);
       } else {
