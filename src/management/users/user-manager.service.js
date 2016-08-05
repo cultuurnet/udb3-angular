fef557ffbe13b16f73cb77a565125e084314ba12
Larry Boltovskoi <larry.bolt@gmail.com>
2016-08-05 10:20:39 +0200

    III-1191: Remove mock for users

--- src/management/users/user-manager.service.js
+++ src/management/users/user-manager.service.js
@@ -30,17 +30,7 @@ function UserManager(udbApi, $q) {
    * @return {Promise.<PagedCollection>}
    */
   service.find = function (query, limit, start) {
-    return $q.when({
-      'itemsPerPage': 30,
-      'totalItems': 3562,
-      'member': [
-        {
-          'uuid': '6f072ba8-c510-40ac-b387-51f582650e27',
-          'email': 'alberto@email.es',
-          'username': 'El Pistolero'
-        }
-      ]});
-    //return udbApi.findUsers(query, limit, start);
+    return udbApi.findUsers(query, limit, start);
   };
 
   /**
