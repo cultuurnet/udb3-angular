eeb6cb21c1923b7f224beeced55e8ab5cbbbfa1e
Reinout Bossuyt <reinout@cultuurnet.be>
2016-10-03 10:39:43 +0200

    III-1333 remove directive time-autocomplete

--- dist/udb3-angular.js
+++ dist/udb3-angular.js
@@ -2185,64 +2185,6 @@ function udbCalendarSummary() {
 
 })();
 
-// Source: src/core/components/time-autocomplete/time-autocomplete.js
-(function () {
-/**
-   * @ngdoc directive
-   * @name udb.core.directive:udbMultiselect
-   * @description
-   * # directive for bootstrap-multiselect integration
-   */
-  angular
-  .module('udb.core')
-  .directive('udbTimeAutocomplete', udbTimeAutocompleteDirective);
-
-  function udbTimeAutocompleteDirective() {
-
-    return {
-      restrict: 'A',
-      link: function(scope) {
-        scope.times = generateTimes();
-      }
-    };
-
-    /**
-     * Generate the time options.
-     */
-    function generateTimes() {
-
-      var increment = 15;
-      var date = new Date(2015, 1, 1, 0, 0);
-      var options = [];
-      var hourLen = 60;
-      var hours = 24;
-
-      for (var i = 0, loopInt = hours * (hourLen / increment); i < loopInt; i++) {
-
-        date.setMinutes(date.getMinutes() + increment);
-        var h = date.getHours();
-        var m = date.getMinutes();
-
-        if (('' + h).length === 1) {
-          h = '0' + h;
-        }
-
-        if (('' + m).length === 1) {
-          m = '0' + m;
-        }
-
-        var label = h + ':' + m;
-        options.push(label);
-      }
-
-      return options;
-
-    }
-
-  }
-
-})();
-
 // Source: src/core/dutch-translations.constant.js
 /**
  * @ngdoc service
