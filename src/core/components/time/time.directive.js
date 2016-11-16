099b5a9e44d08cb25654b9e0282380e3b4c2d302
Stijn Swaanen <swaanenstijn@gmail.com>
2016-11-16 09:24:59 +0100

    III-1333 try to print hour in time input field

--- src/core/components/time/time.directive.js
+++ src/core/components/time/time.directive.js
@@ -15,30 +15,45 @@ function udbTimeDirective() {
   return {
     restrict: 'AE',
     require: 'ngModel',
-    template: '<input type="time" ng-model="ngModel" class="form-control uur" required />',
+    template: '<input type="time" ng-model="hour" class="form-control uur" required />',
     link: link
   };
 
   function link (scope, element, attrs, ngModel) {
-    console.log(ngModel);
-    console.log(element);
 
-    /*ngModel.$render = function() {
-      //element.html(formatter(ngModel.$viewValue));
-      element.html(ngModel.$viewValue);
-    };*/
+    scope.hour = ngModel.$viewValue;
 
     function hoursChanged(timestamp) {
       return formatter(timestamp);
     }
 
+    ngModel.$parsers.push(function(date) {
+      //View -> Model
+      return moment(date).format('HH:mm');
+    });
+
     function formatter(timestamp) {
       //var hour = moment(timestamp);
       //attrs.destination = hour.format('HH:mm');
+      //Model -> View
       return moment(timestamp).format('HH:mm');
     }
 
     ngModel.$formatters.push(formatter);
+
+    ngModel.$render = function() {
+      //element.html(formatter(ngModel.$viewValue));
+      //element.html(ngModel.$viewValue);
+      ngModel.$setViewValue(formatter(ngModel.$modelValue));
+    };
+
+    ngModel.$render = function() {
+      scope.hour = formatter(ngModel.$modelValue);
+    };
+
+
+    console.log(ngModel);
+    console.log(element);
   }
 }
 })();
