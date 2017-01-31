6be9e1673d213ce273b9ba904dab1590f84b7816
IbenTesara <ibenvandeveire@gmail.com>
2017-01-31 09:24:24 +0100

    III-1813 add panel to cultuurkuurcomponent

--- src/cultuurkuur/event-cultuurkuur.component.js
+++ src/cultuurkuur/event-cultuurkuur.component.js
@@ -13,4 +13,10 @@ function EventCultuurKuurComponentController() {
     cm.previewLink = 'http://dev.cultuurkuur.be/agenda/e//' + cm.event.id;
     cm.editLink = 'http://dev.cultuurkuur.be/event/' + cm.event.id + '/edit';
     cm.isIncomplete = (cm.event.educationFields.length === 0 && cm.event.educationLevels.length === 0);
+
+    cm.cultuurKuurInfo = {
+        levels : _.pluck(cm.event.educationLevels,'label'),
+        fields : _.pluck(cm.event.educationFields,'label'),
+        targetAudience : _.pluck(cm.event.educationTargetAudience,'label')
+    };
 }
