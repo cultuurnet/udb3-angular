c17a91c5d2303fc0250d19c29f120d3a1d6b3129
Bram Cordie <bramcordie@gmail.com>
2017-02-17 15:19:29 +0100

    III-1856: Parse combined list of regular and hidden labels for organizers

--- src/core/udb-organizer.factory.spec.js
+++ src/core/udb-organizer.factory.spec.js
@@ -4,6 +4,32 @@ describe('Factory: UDB Organizer', function () {
 
   beforeEach(module('udb.core'));
 
+  var jsonOrganizerWithHiddenLabel = {
+    '@id': 'http://culudb-silex.dev:8080/organizer/357D5297-9E37-1DE9-62398987EA110D38',
+    '@context': '/api/1.0/organizer.jsonld',
+    'name': 'Club Silo',
+    'address': {
+      addressCountry: 'BE',
+      addressLocality: 'Leuven',
+      postalCode: '3000',
+      streetAddress: 'Vaartkom 39'
+    },
+    contactPoint: {
+      'email': [
+        'info@silo.be'
+      ],
+      'phone': [
+        '+32 476 838982'
+      ]
+    },
+    labels: [
+      'green'
+    ],
+    hiddenLabels: [
+      'UiTPAS'
+    ]
+  };
+
   it('should set all the properties when parsing a json organizer', inject(function (UdbOrganizer) {
     var expectedOrganizer = {
       'id': '357D5297-9E37-1DE9-62398987EA110D38',
@@ -55,34 +81,14 @@ describe('Factory: UDB Organizer', function () {
   }));
 
   it('takes into account hidden labels when checking for UiTPAS', inject(function (UdbOrganizer) {
-    var jsonOrganizer = {
-      '@id': 'http://culudb-silex.dev:8080/organizer/357D5297-9E37-1DE9-62398987EA110D38',
-      '@context': '/api/1.0/organizer.jsonld',
-      'name': 'Club Silo',
-      'address': {
-        addressCountry: 'BE',
-        addressLocality: 'Leuven',
-        postalCode: '3000',
-        streetAddress: 'Vaartkom 39'
-      },
-      contactPoint: {
-        'email': [
-          'info@silo.be'
-        ],
-        'phone': [
-          '+32 476 838982'
-        ]
-      },
-      labels: [
-        'green'
-      ],
-      hiddenLabels: [
-        'UiTPAS'
-      ]
-    };
-
-    var organizerFromJson = new UdbOrganizer(jsonOrganizer);
+    var organizerFromJson = new UdbOrganizer(jsonOrganizerWithHiddenLabel);
     expect(organizerFromJson.isUitpas).toEqual(true);
   }));
 
+  it('should combine regular and hidden labels as a single list', inject(function (UdbOrganizer) {
+    var expectedCombinedLabels = ['green', 'UiTPAS'];
+    var organizer = new UdbOrganizer(jsonOrganizerWithHiddenLabel);
+
+    expect(organizer.labels).toEqual(expectedCombinedLabels);
+  }));
 });
