cca410d872ba2835b5c5b9bed9e5cef161a32fcc
Bram Cordie <bramcordie@gmail.com>
2017-03-22 09:43:27 +0100

    III-1617: Uitpas-labels provider with default fallback

--- dist/udb3-angular.js
+++ dist/udb3-angular.js
@@ -19264,6 +19264,42 @@ function UdbUitpasApi($q, $http, appConfig, uitidAuth) {
 }
 UdbUitpasApi.$inject = ["$q", "$http", "appConfig", "uitidAuth"];
 
+// Source: src/uitpas/uitpas-labels.provider.js
+/* jshint sub: true */
+
+/**
+ * @ngdoc service
+ * @name udb.uitpas.UitpasLabelsProvider
+ * @description
+ * # UiTPAS Labels Provider
+ *
+ * All the known UiTPAS labels that link an organizer to card-systems on 2017-03-01 are in the DefaultUitpasLabels
+ * constant. The file used to be updated each time labels changed but now acts as a placeholder.
+ *
+ * The actual labels should be fetched when building or bootstrapping your app and written to the ExtermalUitpasLabels
+ * constant. The UiTPAS service should have an endpoint with all the labels for your environment.
+ * e.g.: https://uitpas.uitdatabank.be/labels for production
+ */
+angular
+  .module('udb.uitpas')
+  .provider('UitpasLabels', UitpasLabelsProvider);
+
+function UitpasLabelsProvider() {
+  var customUitpasLabels;
+
+  /**
+   * Configure the UiTPAS labels by providing a map of {LABEL_KEY: label name}
+   * @param {object} labels
+   */
+  this.useLabels = function(labels) {
+    customUitpasLabels = labels;
+  };
+
+  this.$get = ['DefaultUitpasLabels', function(DefaultUitpasLabels) {
+    return !!customUitpasLabels ? customUitpasLabels : DefaultUitpasLabels;
+  }];
+}
+
 // Source: .tmp/udb3-angular.templates.js
 angular.module('udb.core').run(['$templateCache', function($templateCache) {
 $templateCache.put('templates/calendar-summary.directive.html',
