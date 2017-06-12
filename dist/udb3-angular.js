f2cc9b105d65d8f26fefefb7108f283eedce735f
IbenTesara <ibenvandeveire@gmail.com>
2017-06-12 16:39:36 +0200

    III-1944 Allow single zero on input

--- dist/udb3-angular.js
+++ dist/udb3-angular.js
@@ -21958,7 +21958,7 @@ $templateCache.put('templates/calendar-summary.directive.html',
     "                                    <input type=\"text\"\n" +
     "                                           class=\"form-control\"\n" +
     "                                           name=\"price\"\n" +
-    "                                           ng-pattern=\"/^(0,|[1-9]\\d*|[1-9])(,\\d{1,2})?$/\"\n" +
+    "                                           ng-pattern=\"/^([1-9]\\d*|[1-9]|[0])(,\\d{1,2})?$/\"\n" +
     "                                           ng-model=\"priceInfo.price\"\n" +
     "                                           ng-model-options=\"{ updateOn: 'default' }\"\n" +
     "                                           ng-class=\"{ 'has-error': pfmc.priceForm.priceFieldForm.price.$invalid }\"\n" +
