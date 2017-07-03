356938875fb243444a4ce2676b44249708380e18
Reinout Bossuyt <reinout@cultuurnet.be>
2017-07-03 10:07:12 +0200

    III-2226 ie-fix

--- src/event-detail/ui/event-detail.controller.js
+++ src/event-detail/ui/event-detail.controller.js
@@ -203,7 +203,7 @@ function EventDetail(
     var eventId;
     // When an event is published $scope.eventId is empty,
     // so get the eventId straight from the current url.
-    if (_.isEmpty($scope.eventId)) {
+    if (_.isEmpty($scope.eventId) && Object.keys($scope.eventId).length === 0) {
       eventId = $location.url().split('/')[2];
     }
     else {
