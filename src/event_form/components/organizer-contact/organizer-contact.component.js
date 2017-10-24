0e8dd1db8a323dd7c7600fd485783c81585b5e27
Sambego <sambellen@gmail.com>
2017-10-24 13:28:14 +0200

    Fix indentation

--- src/event_form/components/organizer-contact/organizer-contact.component.js
+++ src/event_form/components/organizer-contact/organizer-contact.component.js
@@ -8,91 +8,91 @@
  * Modal for setting the reservation period.
  */
 angular.module('udb.event-form').component('udbOrganizerContact', {
-    templateUrl: 'templates/organizer-contact.html',
-    controller: OrganizerContactComponent,
-    controllerAs: 'occ',
-    bindings: {
-        contact: '=',
-        onUpdate: '&'
-    }
+  templateUrl: 'templates/organizer-contact.html',
+  controller: OrganizerContactComponent,
+  controllerAs: 'occ',
+  bindings: {
+    contact: '=',
+    onUpdate: '&'
+  }
 });
 
 /* @ngInject */
 function OrganizerContactComponent($scope) {
-    var controller = this;
+  var controller = this;
 
-    controller.newContact = {};
+  controller.newContact = {};
 
+  controller.addingContactEntry = false;
+  controller.validateContact = validateContact;
+  controller.addOrganizerContactEntry = addOrganizerContactEntry;
+  controller.cancelOrganizerContactEntry = cancelOrganizerContactEntry;
+  controller.addOrganizerContactInfo = addOrganizerContactInfo;
+  controller.deleteOrganizerContactInfo = deleteOrganizerContactInfo;
+  controller.sendUpdate = sendUpdate;
+
+  $scope.$on('organizerContactSubmit', function() {
+    controller.organizerContactWrapper.$setSubmitted();
+  });
+
+  function validateContact() {
+    if (
+      _.find(controller.contact, { value: '' }) ||
+      _.find(controller.contact, { value: undefined }) ||
+      controller.organizerContactWrapper.$invalid
+    ) {
+      controller.contactHasErrors = true;
+    } else {
+      controller.contactHasErrors = false;
+    }
+  sendUpdate();
+}
+
+  function resetOrganizerContactEntry() {
+    controller.newContact = {
+      type: '',
+      value: ''
+    };
+  }
+
+  /**
+  * Add a contact info entry for an organizer.
+  */
+  function addOrganizerContactEntry(type) {
+    controller.newContact = {
+      type: type,
+      value: ''
+    };
+    controller.addingContactEntry = true;
+  }
+
+  /**
+  * Add a contact info entry for an organizer.
+  */
+  function cancelOrganizerContactEntry() {
+    resetOrganizerContactEntry();
     controller.addingContactEntry = false;
-    controller.validateContact = validateContact;
-    controller.addOrganizerContactEntry = addOrganizerContactEntry;
-    controller.cancelOrganizerContactEntry = cancelOrganizerContactEntry;
-    controller.addOrganizerContactInfo = addOrganizerContactInfo;
-    controller.deleteOrganizerContactInfo = deleteOrganizerContactInfo;
-    controller.sendUpdate = sendUpdate;
+  }
 
-    $scope.$on('organizerContactSubmit', function() {
-        controller.organizerContactWrapper.$setSubmitted();
-    });
-
-    function validateContact() {
-        if (
-            _.find(controller.contact, { value: '' }) ||
-            _.find(controller.contact, { value: undefined }) ||
-            controller.organizerContactWrapper.$invalid
-        ) {
-            controller.contactHasErrors = true;
-        } else {
-            controller.contactHasErrors = false;
-        }
-        sendUpdate();
-    }
-
-    function resetOrganizerContactEntry() {
-        controller.newContact = {
-            type: '',
-            value: ''
-        };
-    }
-
-    /**
-   * Add a contact info entry for an organizer.
-   */
-    function addOrganizerContactEntry(type) {
-        controller.newContact = {
-            type: type,
-            value: ''
-        };
-        controller.addingContactEntry = true;
-    }
-
-    /**
-   * Add a contact info entry for an organizer.
-   */
-    function cancelOrganizerContactEntry() {
+  /* */
+  function addOrganizerContactInfo() {
+    validateContact();
+      if (!controller.contactHasErrors) {
+        controller.contact.push(controller.newContact);
         resetOrganizerContactEntry();
         controller.addingContactEntry = false;
-    }
+      }
+  }
 
-    /* */
-    function addOrganizerContactInfo() {
-        validateContact();
-        if (!controller.contactHasErrors) {
-            controller.contact.push(controller.newContact);
-            resetOrganizerContactEntry();
-            controller.addingContactEntry = false;
-        }
-    }
+  /**
+  * Remove a given key of the contact info.
+  */
+  function deleteOrganizerContactInfo(index) {
+    controller.contact.splice(index, 1);
+    validateContact();
+  }
 
-    /**
-   * Remove a given key of the contact info.
-   */
-    function deleteOrganizerContactInfo(index) {
-        controller.contact.splice(index, 1);
-        validateContact();
-    }
-
-    function sendUpdate() {
-        controller.onUpdate({ error: controller.contactHasErrors });
-    }
+  function sendUpdate() {
+    controller.onUpdate({ error: controller.contactHasErrors });
+  }
 }
