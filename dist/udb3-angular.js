72c282d5ea624a7edbb0599793ea93e6a7c9b464
Luc Wollants <luc.wollants@apptite.be>
2018-01-19 13:10:50 +0100

    III-2392 Fix typos foutboodschappen UiTPAS.

--- dist/udb3-angular.js
+++ dist/udb3-angular.js
@@ -3208,9 +3208,9 @@ angular.module('udb.core')
         'uitpas': 'UiTPAS',
         'uitpas_alert': 'Dit is een UiTPAS organisator. Selecteer een prijs om specifieke UiTPAS-informatie toe te voegen.',
         'uitpas_info': 'Dit is een UiTPAS activiteit.',
-        'cantChangePrice': 'Voor dit evenement zijn al tickets verkocht met de bestaande prijsinformatie. Je kan de prijsinformatie niet meer wijzigen.',
+        'cantChangePrice': 'Voor dit evenement zijn al UiTPAS-tickets verkocht met de bestaande prijsinformatie. Je kan de prijsinformatie niet meer wijzigen.',
         'cantChangeOrganiser': 'Voor dit evenement zijn al UiTPAS-tickets verkocht. Je kan de organisatie niet meer wijzigen.',
-        'unavailable': 'kan UiTPAS momenteel niet bereiken, probeer het later opnieuw of contacteer de helpdesk (vragen@uitdatabank.be)'
+        'unavailable': 'UiTPAS kan momenteel niet bereikt worden, probeer het later opnieuw of contacteer de helpdesk (vragen@uitdatabank.be).'
       },
       cardSystems: {
         'card_systems': 'Kaartsystemen',
