5c04ed07fdd5c1ca13084363d7061aa477914901
Stijn Swaanen <swaanenstijn@gmail.com>
2019-01-21 10:40:56 +0100

    III-2842 maintenance mode and text for cultuurkuur plugin

--- src/cultuurkuur/event-cultuurkuur.component.js
+++ src/cultuurkuur/event-cultuurkuur.component.js
@@ -15,27 +15,35 @@ angular
  * @ngInject
  */
 function EventCultuurKuurComponentController(appConfig) {
-  var cm = this,
-      cultuurkuurUrl = _.get(appConfig, 'cultuurkuurUrl');
+  var cm = this;
+  cm.cultuurkuurMaintenance = _.get(appConfig, 'cultuurkuur.maintenance');
 
-  cm.previewLink = cultuurkuurUrl + 'agenda/e//' + cm.event.id + getUTMParameters('preview1.0');
-  cm.editLink = cultuurkuurUrl + 'event/' + cm.event.id + '/edit' + getUTMParameters('edit1.0');
-  cm.continueLink = cultuurkuurUrl + 'event/' + cm.event.id + '/edit' + getUTMParameters('continue1.0');
-  cm.isIncomplete = (cm.event.educationFields.length === 0 && cm.event.educationLevels.length === 0);
+  if (!cm.cultuurkuurMaintenance) {
+    var cultuurkuurUrl = _.get(appConfig, 'cultuurkuur.cultuurkuurUrl');
 
-  cm.cultuurKuurInfo = {
-    levels : _.pluck(cm.event.educationLevels, 'label'),
-    fields : _.pluck(cm.event.educationFields, 'label'),
-    targetAudience : _.pluck(cm.event.educationTargetAudience, 'label')
-  };
+    cm.previewLink = cultuurkuurUrl + 'agenda/e//' + cm.event.id + getUTMParameters('preview1.0');
+    cm.editLink = cultuurkuurUrl + 'event/' + cm.event.id + '/edit' + getUTMParameters('edit1.0');
+    cm.continueLink = cultuurkuurUrl + 'event/' + cm.event.id + '/edit' + getUTMParameters('continue1.0');
+    cm.isIncomplete = (cm.event.educationFields.length === 0 && cm.event.educationLevels.length === 0);
 
-  cm.forSchools = cm.event.audience.audienceType === 'education';
+    cm.cultuurKuurInfo = {
+      levels : _.pluck(cm.event.educationLevels, 'label'),
+      fields : _.pluck(cm.event.educationFields, 'label'),
+      targetAudience : _.pluck(cm.event.educationTargetAudience, 'label')
+    };
+
+    cm.forSchools = cm.event.audience.audienceType === 'education';
+  }
+  else {
+    cm.cultuurkuurMessage = _.get(appConfig, 'cultuurkuur.cultuurkuurMessage');
+  }
+
 
   function getUTMParameters(type) {
     return '?utm_source=uitdatabank.be' +
-    '&utm_medium=referral' +
-    '&utm_campaign=udb3' +
-    '&utm_content=' +
-    type;
+        '&utm_medium=referral' +
+        '&utm_campaign=udb3' +
+        '&utm_content=' +
+        type;
   }
 }
