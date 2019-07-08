3c2e1d1fcc4897ea7a1dc966b973a4ed7c1b0f8f
Bert Ramakers <bert@madewithlove.be>
2019-07-08 13:29:48 +0200

    Revert "III-2944 Temporarily revert only sending the location id when creating a new event"
    This reverts commit b5c9e8b56d607ccc8fd45c0c36133a64534ff049.

--- src/entry/crud/event-crud.service.js
+++ src/entry/crud/event-crud.service.js
@@ -33,6 +33,10 @@ function EventCrud(
       return (_.isDate(property) || !_.isEmpty(property)) && !isStream;
     });
 
+    if (majorInfo.location && majorInfo.location.id) {
+      majorInfo.location = majorInfo.location.id;
+    }
+
     return majorInfo;
   }
 
