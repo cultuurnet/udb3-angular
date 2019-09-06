44a2b92cd141806c687f24e5dd16192b45e30568
brampauwelyn <brampauw@gmail.com>
2019-09-06 13:51:30 +0200

    III-2979 - fix tests + added new test for bookable event

--- src/event_form/components/audience/form-audience.controller.spec.js
+++ src/event_form/components/audience/form-audience.controller.spec.js
@@ -19,15 +19,34 @@ describe('Controller: Form: Audience', function () {
   }
 
   it('should be enabled if the current form data is an event', function () {
-    var formData = { isEvent: true },
-        controller = getController(formData);
+    var formData = { isEvent: true };
+    formData.getLocation = function() {
+      this.isDummyPlaceForEducationEvents = false;
+      return this; 
+    };
+    var controller = getController(formData);
 
     expect(controller.enabled).toEqual(true);
   });
 
   it('should load the audience type for the event in form data', function () {
-    var formData = { isEvent: true, audienceType: 'education' },
-      controller = getController(formData);
+    var formData = { isEvent: true, audienceType: 'education' };
+    formData.getLocation = function() {
+      this.isDummyPlaceForEducationEvents = false;
+      return this; 
+    };
+    var controller = getController(formData);
+
+    expect(controller.audienceType).toEqual('education');
+  });
+
+  it('for a bookable event should be education', function () {
+    var formData = { isEvent: true };
+    formData.getLocation = function() {
+      this.isDummyPlaceForEducationEvents = true;
+      return this; 
+    };
+    var controller = getController(formData);
 
     expect(controller.audienceType).toEqual('education');
   });
@@ -36,8 +55,12 @@ describe('Controller: Form: Audience', function () {
     var formData = {
           isEvent: true,
           apiUrl: new URL('http://du.de/event/1da2bb3c-616f-4e89-9b17-f142413046d2')
-        },
-        controller = getController(formData);
+        };
+    formData.getLocation = function() {
+      this.isDummyPlaceForEducationEvents = false;
+      return this; 
+    };
+    var controller = getController(formData);
 
     controller.setAudienceType('education');
 
