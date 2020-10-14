18982a258378f3f69d61eac6c282912683450218
SImon de Bruijn <simon.debruijn@publiq.be>
2020-10-14 15:07:43 +0200

    generate dist files

--- dist/udb3-angular.js
+++ dist/udb3-angular.js
@@ -15314,6 +15314,7 @@ function EventFormStep4Controller(
 
     eventCrudPromise.then(function(newEventFormData) {
       EventFormData = newEventFormData;
+      eventCrud.updateTypicalAgeRange(EventFormData);
       EventFormData.majorInfoChanged = false;
 
       $scope.saving = false;
