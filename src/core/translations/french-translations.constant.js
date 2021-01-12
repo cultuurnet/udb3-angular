2e7fb9a94026d556a456969edcac31c310767319
Simon de Bruijn <simon.debruijn@publiq.be>
2021-01-12 16:25:17 +0100

    Change extra information

--- src/core/translations/french-translations.constant.js
+++ src/core/translations/french-translations.constant.js
@@ -545,7 +545,7 @@ angular.module('udb.core')
         'title': 'Titre',
         'description': 'Description',
         'add_text': 'Ajouter texte',
-        'required_200': 'Pour attirer un nouveau public, les 200 premiers symboles sont les plus importants.',
+        'required_200': 'Pour attirer un nouveau public, les 200 premier caractères sont les plus importants. Encore 199 caractères.',
         'required_still': 'Encore',
         'required_signs': 'symboles.',
         'required_200_help': 'Intégrez le message le plus important dans les 200 premiers symboles. Vous pouvez ensuite ajouter des informations générales.',
@@ -605,7 +605,7 @@ angular.module('udb.core')
         'main_image': 'Créer image principale',
         'add_image': 'Ajouter une image',
         priceInfo: {
-          'price_label': 'Prix',
+          'price_label': 'Tarif',
           'add_prices': 'Ajouter prix',
           'free': 'Gratuit',
           'prices': 'Prix',
