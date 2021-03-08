be325a04637963a112b13a49f8310fc5b82348c4
Luc Wollants <lucwollants@gmail.com>
2021-03-08 09:26:02 +0100

    Pick opening hours from root and add to calendar
    The clone was removed because `majorInfo` was already a new object

--- src/entry/crud/event-crud.service.js
+++ src/entry/crud/event-crud.service.js
@@ -34,10 +34,10 @@ function EventCrud(
       majorInfo.location = majorInfo.location.id;
     }
 
-    var majorInfoWithStatus = _.cloneDeep(majorInfo);
-    majorInfoWithStatus.calendar.status = majorInfo.status;
+    majorInfo.calendar.status = majorInfo.status;
+    majorInfo.calendar.openingHours = majorInfo.openingHours;
 
-    return majorInfoWithStatus;
+    return majorInfo;
   }
 
   /**
