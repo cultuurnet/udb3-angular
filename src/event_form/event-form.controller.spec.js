aef0de1825159b6a0756f25bdc91b0125d1ba666
Luc Wollants <lucwollants@gmail.com>
2021-09-01 15:15:15 +0200

    Asure booking is available after a copy
    See also https://github.com/cultuurnet/udb3-angular/pull/686

--- src/event_form/event-form.controller.spec.js
+++ src/event_form/event-form.controller.spec.js
@@ -106,7 +106,8 @@ describe('Controller: Event Form', function() {
         allDay: false,
         start: moment('2017-09-10T10:00').toISOString(),
         end: moment('2017-09-10T18:00').toISOString(),
-        status: {type: 'Available'}
+        status: {type: 'Available'},
+        bookingAvailability: {type: 'Available'}
       }
     ];
 
