8ab73d6f9161a76ba5ba927e8b53206d1f89253b
bram <bram.pauwelyn@publiq.be>
2022-02-18 14:42:37 +0100

    III-4589 - Added extra check for when appConfig.addOffer is undefined

--- src/event_form/event-form-data.factory.js
+++ src/event_form/event-form-data.factory.js
@@ -549,7 +549,7 @@ function EventFormDataFactory(rx, calendarLabels, moment, OpeningHoursCollection
 
       if (formData.calendar.calendarType === 'periodic') {
         formData.calendar.startDate = moment().startOf('day').toDate();
-        if (appConfig.addOffer.defaultEndPeriod) {
+        if (appConfig.addOffer && appConfig.addOffer.defaultEndPeriod) {
           var defaultEndPeriod = appConfig.addOffer.defaultEndPeriod;
           formData.calendar.endDate =
               moment(formData.calendar.startDate).add(defaultEndPeriod, 'd').startOf('day').toDate();
