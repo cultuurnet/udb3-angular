c275ce6c5e8eda43b9a7a7bf039b93af2e158bfc
Emma Fabre <ehtnam6@gmail.com>
2024-01-23 16:06:03 +0100

    Add values to options

--- src/entry/labelling/offer-languages-modal.controller.js
+++ src/entry/labelling/offer-languages-modal.controller.js
@@ -14,10 +14,31 @@ angular
 /* @ngInject */
 function OfferLanguagesModalCtrl($uibModalInstance) {
   var lmc = this;
+  lmc.close = close;
+  lmc.ok = ok;
+
   lmc.options = [
-    {label: 'één taalicoon', text: 'om te kunnen deelnemen, moet je nog niet veel Nederlands spreken of gebruiken.'},
-    {label: 'twee taaliconen', text: 'deelnemers begrijpen al een beetje Nederlands maar spreken het nog niet zo goed.'},
-    {label: 'drie taaliconen', text: 'deelnemers spreken vrij veel Nederlands en kunnen ook iets vertellen.'},
-    {label: 'vier taaliconen', text: 'deelnemers begrijpen Nederlands en spreken het ook goed.'},
+    {label: 'één taalicoon', value: 'één taalicoon', text: 'om te kunnen deelnemen, moet je nog niet veel Nederlands spreken of gebruiken.'},
+    {label: 'twee taaliconen', value: 'twee taaliconen', text: 'deelnemers begrijpen al een beetje Nederlands maar spreken het nog niet zo goed.'},
+    {label: 'drie taaliconen', value: 'drie taaliconen', text: 'deelnemers spreken vrij veel Nederlands en kunnen ook iets vertellen.'},
+    {label: 'vier taaliconen', value: 'vier taaliconen', text: 'deelnemers begrijpen Nederlands en spreken het ook goed.'},
   ];
+
+  function close() {
+    $uibModalInstance.dismiss('cancel');
+  }
+
+  function ok() {
+    var selected = lmc.options.filter(function (option) {
+      return option.selected;
+    }).map(function (option) {
+      return option.value;
+    });
+
+    if (!selected.length) {
+      return;
+    }
+
+    $uibModalInstance.close(selected);
+  }
 }
